4c4b0a0ca2247db828019181a4d669710d18c951
Shubham Sanap <sanapshubham305@gmail.com>
2023-07-31 16:19:03 +0530

    while loop examples
    while loop examples handsOn

---
+++ 06_while_Assignment.js
@@ -0,0 +1,83 @@
+console.log(
+  "================== wap to print 5-15 ======================================================="
+);
+
+var index = 5;
+var result = " ";
+while (index <= 15) {
+  result = result.concat(index).concat(" ");
+  index++;
+}
+console.log(result);
+
+console.log(
+  "================== wap to print 50-40 ======================================================="
+);
+
+var index = 50;
+var result = " ";
+while (index >= 40) {
+  result = result.concat(index).concat(" ");
+  index--;
+}
+console.log(result);
+
+console.log(
+  "================== wap to print odd numbers ======================================================="
+);
+
+var index = 1;
+var result = " ";
+while (index <= 30) {
+  result = result.concat(index).concat(" ");
+  index = index + 2;
+}
+console.log(result);
+
+console.log(
+  "==================wap to print even number ======================================================="
+);
+
+var index = 2;
+var result = "";
+while (index <= 20) {
+  result = result.concat(index).concat(" ");
+  index = index + 2;
+}
+console.log(result);
+
+console.log(
+  "================== wap to print 5 table ======================================================="
+);
+
+var index = 5;
+var result = " ";
+while (index <= 50) {
+  result = result.concat(index).concat(" ");
+  index = index + 5;
+}
+console.log(result);
+
+console.log(
+  "================== wap to print 10 table ======================================================="
+);
+
+var index = 10;
+var result = "";
+while (index <= 100) {
+  result = result.concat(index).concat(" ");
+  index = index + 10;
+}
+console.log(result);
+
+console.log(
+  "================== wap to print 10 table  reverse ======================================================="
+);
+
+var index = 100;
+var result = " ";
+while (index >= 10) {
+  result = result.concat(index).concat(" ");
+  index = index - 10;
+}
+console.log(result);
