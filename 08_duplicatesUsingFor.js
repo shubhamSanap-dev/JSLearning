9a39a3a7c927586d730083e5d4d8bed9d72a05f5
Shubham Sanap <sanapshubham305@gmail.com>
2023-08-06 15:41:26 +0530

    object practice
    object practice and duplicates code alternative

--- 08_duplicatesUsingFor.js
+++ 08_duplicatesUsingFor.js
@@ -1,13 +1,39 @@
 // remove duplicates using for loop only
 let array = [4, 5, 4, 5, 8, 5, 7, 8, 9, 7, 8];
 
-let uniqueArr = [];
+ let uniqueArr = [];
+ for(let i=0; i<array.length; i++){
+     if(
+         uniqueArr.indexOf(array[i])== -1
+     ){
+         uniqueArr.push(array[i]);
+    }
+ }
+
+ console.log(uniqueArr);
+
+const evenNum=[];
 for(let i=0; i<array.length; i++){
-    if(
-        uniqueArr.indexOf(array[i])== -1
-    ){
-        uniqueArr.push(array[i]);
+    if (array[i]%2==0) {
+        evenNum.push(array[i]);
     }
 }
+console.log(evenNum);
 
-console.log(uniqueArr);
+let a=[4,5,6,4,5,8,5,7,8,9,7,8];
+let b=[]
+for(i=0;i<a.length;i++){
+    for(j=0;j<a.length;j++){
+        if(b.includes(a[i])){
+            
+
+            continue;
+        }
+        else{
+            b.push(a[i])
+
+        }
+
+    }
+}
+console.log(b);
