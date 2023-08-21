5783ef131c334cd18d00a6f4d108021ca44ffda9
Shubham Sanap <sanapshubham305@gmail.com>
2023-08-21 11:51:18 +0530

    ES6 features
    ES6 features practice

---
+++ 17_functionDefaultParameter.js
@@ -0,0 +1,10 @@
+
+
+function division(num1, num2){
+ console.log(num1/num2);
+}
+
+division(4, 5);
+division(0 , 7);
+division(8, 0);
+division(0, 0);
---
+++ 19_selfInvokingFun.js
@@ -0,0 +1,7 @@
+// Self invoking function or IIFE - Immediately invoked function expression
+
+( function(){
+
+    console.log(`inside display function..`);
+
+} )();
